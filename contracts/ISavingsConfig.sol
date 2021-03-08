bc2a935b8040568a1c63e6541fa1b245fdf974d4
rony4d <ugochukwuaronu@gmail.com>
2021-03-08 08:23:16 +0100

    Changed only smart contract addresses to support Venus on BSC. Code compiled successfully

---
+++ contracts/ISavingsConfig.sol
@@ -0,0 +1,41 @@
+pragma solidity 0.6.6;
+
+import "./ISavingsConfigSchema.sol";
+
+interface ISavingsConfig is ISavingsConfigSchema{
+
+    function getRuleSet(string calldata ruleKey)
+        external
+        returns (
+            uint256,
+            uint256,
+            uint256,
+            bool,
+            RuleDefinition
+        );
+
+    function getRuleManager(string calldata ruleKey) external returns (address);
+
+    function changeRuleCreator(string calldata ruleKey, address newRuleManager)
+        external;
+
+    function createRule(
+        string calldata ruleKey,
+        uint256 minimum,
+        uint256 maximum,
+        uint256 exact,
+        RuleDefinition ruleDefinition
+    ) external;
+
+    function modifyRule(
+        string calldata ruleKey,
+        uint256 minimum,
+        uint256 maximum,
+        uint256 exact,
+        RuleDefinition ruleDefinition
+    ) external;
+
+    function disableRule(string calldata ruleKey) external;
+
+    function enableRule(string calldata ruleKey) external;
+}
---
+++ contracts/ITreasury.sol
@@ -0,0 +1,13 @@
+pragma solidity 0.6.6;
+
+interface ITreasury {
+    function depositToken(address token) external;
+
+    function getEtherBalance() external view returns (uint256);
+
+    function getTokenBalance(address token) external view returns (uint256);
+
+    function withdrawEthers(uint256 amount) external;
+
+    function withdrawTokens(address tokenAddress, uint256 amount) external;
+}
